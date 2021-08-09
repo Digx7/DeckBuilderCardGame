09a18f5e629ade53d7de97e3e80208411941b4bd
Digx7 <37136301+Digx7@users.noreply.github.com>
2021-08-09 15:44:15 -0700

    Last Changes

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+  protected string name {get; set;}
+  protected int cost {get; set;}
+
+
+}
---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    Lands land;
+
+    public void Awake(){
+      land = new Lands();
+      land.typeOfLand = "I hate this";
+    }
+
+    public void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.L))
+      {
+        Debug.Log ("The name of the land should be " + land.typeOfLand);
+      }
+    }
+}
